9192bfb9cbe93cc8bb85f8c13c51a849e47d3e98
tzvaita <tzvaita@gmail.com>
2020-02-28 23:35:24 +0200

    added main bot functionality in microbot ruby file

---
+++ microbot.rb
@@ -0,0 +1,32 @@
+require 'telegram_bot'
+token = '1135906001:AAEnsoxIHkfwQ2WXtUo58wAgS7x7cwOvxQo'
+bot = TelegramBot.new(token: token)
+
+bot.get_updates(fail_silently: true) do |message|
+
+    puts "@#{message.from.username}: #{message.text}"
+    command = message.get_command_for(bot)
+  
+    message.reply do |reply|
+        case command
+        when /start/i
+            reply.text = "Hello, #{message.from.first_name}, to Tennyson Takudzwa Zvaita info bot. What would you like to know about?
+            - /aboutme
+            - /skills
+            - /experience
+            - /education
+            - /projects
+            - /contact"
+        when /skills/i
+            skills = %w[Html CSS Ruby Rails Javascript Git]
+            all_skills = skills.each {|i| puts i.upcase}
+            reply.text = "#{all_skills}"
+        when /learn/i
+            reply.text = 'To search for information from RubyDocs type what you want to search for'
+        else
+            reply.text = "I have no idea what #{command.inspect} means."
+        end
+        puts "sending #{reply.text.inspect} to @#{message.from.username}"
+        reply.send_with(bot)
+    end
+end
